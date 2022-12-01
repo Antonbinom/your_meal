3e3a0dd491f575869b2de006e18780e56f710296
Anton Semenikhin <protest_the_hero@mail.ru>
2022-12-01 10:45:46 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -7,6 +7,24 @@ import {setModalDescription} from './setModalDescription.js'
 import {renderListProduct} from './renderListProduct.js';
 import {navigationListController} from './navigationListController.js';
 
+const burger = {
+	"id": "3184803604",
+	"title": "Мясная бомба",
+	"price": 689,
+	"weight": 520,
+	"calories": 430,
+	"description": "Супер мясное наслаждение! Большая рубленая котлета из свежего говяжего мяса покорит вас своей сочностью, а хрустящие листья салата добавят свежести.",
+	"ingredients": [
+		"Пшеничная булочка",
+		"Котлета из говядины",
+		"Красный лук",
+		"Листья салата",
+		"Соус горчичный"
+	],
+	"category": "burger",
+	"image": "/img/megaburger.jpg"
+};
+
 catalogList.addEventListener('click', (e) => {
 	const target = e.target;
 	if (target.closest('.product__title') || e.target.classList.contains('product__image')) {
